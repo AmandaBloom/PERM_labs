84279b8a927f44001c11cb3c19df9117d9b1d7af
Tomasz Pawlak <t.pawlak@stud.elka.pw.edu.pl>
2023-03-23 16:48:08 +0100

    Delete highpass.m

--- Lab45/highpass.m
+++
@@ -1,8 +0,0 @@
-function filter = highpass(x, fc, bw, fs)
-    filter = lowpass(x, fc, bw, fs);
-    M = round(fs*4/bw);
-
-    filter = -1*filter;
-    filter(M/2+1) = filter(M/2+1) + 1;
-    
-end
